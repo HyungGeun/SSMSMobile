e1eeb624faa30ef8e8e911fbb3d2631f14576086
SSMS <ege6066@naver.com>
2016-07-11 17:53:21 +0900

    zzd

--- app/src/main/java/com/ktds/ssms/ssmsmobile/MainActivity.java
+++ app/src/main/java/com/ktds/ssms/ssmsmobile/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 // Jo Hyung Geun
+//asd
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
