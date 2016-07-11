3b57103e8033f577572dd5fc12757871cb21cdb6
SSMS <ege6066@naver.com>
2016-07-11 17:49:29 +0900

    zz

--- app/src/main/java/com/ktds/ssms/ssmsmobile/MainActivity.java
+++ app/src/main/java/com/ktds/ssms/ssmsmobile/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Jo Hyung Geun
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
